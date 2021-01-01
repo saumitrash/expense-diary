c9b51bb104d9a7f2f8f5c1bb77cccef787dc1293
darkyed <saumitra.sharma@iitgn.ac.in>
2021-01-01 16:03:17 +0530

    added some  tests for update view

--- expenses/tests.py
+++ expenses/tests.py
@@ -3,7 +3,6 @@ from django.urls import reverse
 
 from datetime import timedelta
 from django.utils import timezone
-from calendar import monthrange
 # Create your tests here.
 
 from .models import Expense
@@ -11,17 +10,14 @@ from .models import Expense
 
 def add_expense(amount=0, days=0, title="default title", desc="default desc"):
     time = timezone.now() + timedelta(days=days)
-    
-    return Expense.objects.create(
-        amount=amount,
-        payment_time=time,
-        title=title,
-        description=desc
-    )
+
+    return Expense.objects.create(amount=amount,
+                                  payment_time=time,
+                                  title=title,
+                                  description=desc)
 
 
 class ExpenseModelTests(TestCase):
-
     def test_was_published_recently_with_future_expense(self):
         """
         was_published_recently() returns False for expenses whose pub_date
@@ -31,7 +27,6 @@ class ExpenseModelTests(TestCase):
         future_expense = Expense(payment_time=time)
 
         self.assertIs(future_expense.was_payed_recently(), False)
-    
 
     def test_was_published_recently_with_old_expense(self):
         """
@@ -41,31 +36,33 @@ class ExpenseModelTests(TestCase):
         now = timezone.now()
         today = now.day
 
-        time = now - timedelta(days=(today+1), seconds=1)
-        
+        time = now - timedelta(days=(today + 1), seconds=1)
+
         old_expense = Expense(payment_time=time)
-        
+
         self.assertIs(old_expense.was_payed_recently(), False)
 
 
 class ExpenseHomeViewTests(TestCase):
     def test_home_view_redirect(self):
         now = timezone.now()
-        month,year = now.month, now.year
+        month, year = now.month, now.year
         expected_path = '/expense/%d/%d/' % (year, month)
         response = self.client.get(reverse('expenses:home'))
-        self.assertRedirects(response, expected_url=expected_path,\
-            target_status_code=200, fetch_redirect_response=True)
-        
+        self.assertRedirects(response,
+                             expected_url=expected_path,
+                             target_status_code=200,
+                             fetch_redirect_response=True)
+
 
 class ExpenseIndexViewTests(TestCase):
     def test_past_expense_route_edge(self):
         # now = timezone.now()
         # month, year = now.month, now.year
-        month, year = 1,2021
-        response = self.client.get(reverse('expenses:index', args=(year, month)))
-        self.assertEqual(response.context['prev'], {'year':2020, 'month':12})
-
+        month, year = 1, 2021
+        response = self.client.get(
+            reverse('expenses:index', args=(year, month)))
+        self.assertEqual(response.context['prev'], {'year': 2020, 'month': 12})
 
     def test_no_expenses(self):
         """
@@ -73,11 +70,11 @@ class ExpenseIndexViewTests(TestCase):
         """
         now = timezone.now()
         month, year = now.month, now.year
-        response = self.client.get(reverse('expenses:index', args=(year,month)))
+        response = self.client.get(
+            reverse('expenses:index', args=(year, month)))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No expenses were found")
         self.assertQuerysetEqual(response.context['page_obj'], [])
-    
 
     def test_past_expense_on_present_index(self):
         """
@@ -86,16 +83,14 @@ class ExpenseIndexViewTests(TestCase):
         """
         now = timezone.now()
         day, month, year = now.day, now.month, now.year
-        
-        delta_days = -(day+1)
-        
+
+        delta_days = -(day + 1)
+
         add_expense(amount=500, days=delta_days)
-        response = self.client.get(reverse('expenses:index', args=(year, month)))
-        self.assertQuerysetEqual(
-            response.context['page_obj'],
-            []
-        )
-    
+        response = self.client.get(
+            reverse('expenses:index', args=(year, month)))
+        self.assertQuerysetEqual(response.context['page_obj'], [])
+
     def test_past_expense_on_past_index(self):
         """
         expenses with a payment_date in the past are
@@ -103,23 +98,22 @@ class ExpenseIndexViewTests(TestCase):
         """
         now = timezone.now()
         day, month, year = now.day, now.month, now.year
-        
-        delta_days = -(day+1)
-        
+
+        delta_days = -(day + 1)
+
         add_expense(amount=500, days=delta_days)
         old_time = now + timedelta(days=delta_days)
         old_year, old_month = old_time.year, old_time.month
 
-        response = self.client.get(reverse('expenses:index', args=(old_year, old_month)))
+        response = self.client.get(
+            reverse('expenses:index', args=(old_year, old_month)))
         self.assertNotContains(response, 'Add Expense')
         self.assertEqual(response.status_code, 200)
 
         self.assertTrue(len(response.context['page_obj']) == 1)
-        self.assertQuerysetEqual(
-            response.context['page_obj'],
-            ['<Expense: 500.0>']
-        )
-    
+        self.assertQuerysetEqual(response.context['page_obj'],
+                                 ['<Expense: 500.0>'])
+
     def test_present_expense_with_no_past_expense(self):
         """
         expenses with a payment_date in the past are not
@@ -128,22 +122,21 @@ class ExpenseIndexViewTests(TestCase):
         """
         now = timezone.now()
         day, month, year = now.day, now.month, now.year
-        
+
         add_expense(amount=500)
 
-        response = self.client.get(reverse('expenses:index', args=(year, month)))
-        
+        response = self.client.get(
+            reverse('expenses:index', args=(year, month)))
+
         self.assertEqual(response.status_code, 200)
-        
+
         self.assertContains(response, "badge-expended")
         self.assertContains(response, "more than last month")
 
         self.assertTrue(len(response.context['page_obj']) == 1)
-        
-        self.assertQuerysetEqual(
-            response.context['page_obj'],
-            ['<Expense: 500.0>']
-        )
+
+        self.assertQuerysetEqual(response.context['page_obj'],
+                                 ['<Expense: 500.0>'])
 
     def test_present_expense_with_more_past_expense(self):
         """
@@ -152,24 +145,23 @@ class ExpenseIndexViewTests(TestCase):
         """
         now = timezone.now()
         day, month, year = now.day, now.month, now.year
-        
-        add_expense(amount=500)
-        add_expense(amount=1000, days=-(day+1))
 
-        response = self.client.get(reverse('expenses:index', args=(year, month)))
-        
+        add_expense(amount=500)
+        add_expense(amount=1000, days=-(day + 1))
+
+        response = self.client.get(
+            reverse('expenses:index', args=(year, month)))
+
         self.assertEqual(response.status_code, 200)
-        
+
         self.assertContains(response, "badge-saved")
         self.assertContains(response, "less than last month")
 
         self.assertTrue(len(response.context['page_obj']) == 1)
-        
-        self.assertQuerysetEqual(
-            response.context['page_obj'],
-            ['<Expense: 500.0>']
-        )
-    
+
+        self.assertQuerysetEqual(response.context['page_obj'],
+                                 ['<Expense: 500.0>'])
+
     def test_present_expense_with_less_past_expense(self):
         """
         More expense in the present month compared to last month
@@ -177,24 +169,23 @@ class ExpenseIndexViewTests(TestCase):
         """
         now = timezone.now()
         day, month, year = now.day, now.month, now.year
-        
-        add_expense(amount=500)
-        add_expense(amount=200, days=-(day+1))
 
-        response = self.client.get(reverse('expenses:index', args=(year, month)))
-        
+        add_expense(amount=500)
+        add_expense(amount=200, days=-(day + 1))
+
+        response = self.client.get(
+            reverse('expenses:index', args=(year, month)))
+
         self.assertEqual(response.status_code, 200)
-        
+
         self.assertContains(response, "badge-expended")
         self.assertContains(response, "more than last month")
 
         self.assertTrue(len(response.context['page_obj']) == 1)
-        
-        self.assertQuerysetEqual(
-            response.context['page_obj'],
-            ['<Expense: 500.0>']
-        )
-        
+
+        self.assertQuerysetEqual(response.context['page_obj'],
+                                 ['<Expense: 500.0>'])
+
     def test_present_expense_with_same_past_expense(self):
         """
         Same expenses in the present and past month should
@@ -202,75 +193,78 @@ class ExpenseIndexViewTests(TestCase):
         """
         now = timezone.now()
         day, month, year = now.day, now.month, now.year
-        
-        add_expense(amount=500)
-        add_expense(amount=500, days=-(day+1))
 
-        response = self.client.get(reverse('expenses:index', args=(year, month)))
-        
+        add_expense(amount=500)
+        add_expense(amount=500, days=-(day + 1))
+
+        response = self.client.get(
+            reverse('expenses:index', args=(year, month)))
+
         self.assertEqual(response.status_code, 200)
-        
+
         self.assertContains(response, "badge-same")
         self.assertContains(response, "Same as last month")
         self.assertNotContains(response, "Rs. 0 Same as last month")
 
         self.assertTrue(len(response.context['page_obj']) == 1)
-        
-        self.assertQuerysetEqual(
-            response.context['page_obj'],
-            ['<Expense: 500.0>']
-        )
 
-    def test_present_expense_with_multiple_expenses_less_than_page_objects(self):
+        self.assertQuerysetEqual(response.context['page_obj'],
+                                 ['<Expense: 500.0>'])
+
+    def test_present_expense_with_multiple_expenses_less_than_page_objects(
+            self):
         """
         expenses items are less than the paginated items allowed
         on the page, so all of them should be displayed on the page
         """
         now = timezone.now()
         day, month, year = now.day, now.month, now.year
-        
-        for i in range(3):
-            add_expense(amount=(500*(i+1)))
 
-        response = self.client.get(reverse('expenses:index', args=(year, month)))
-        
+        for i in range(3):
+            add_expense(amount=(500 * (i + 1)))
+
+        response = self.client.get(
+            reverse('expenses:index', args=(year, month)))
+
         self.assertEqual(response.status_code, 200)
-        
+
         self.assertContains(response, "badge-expended")
         self.assertContains(response, "more than last month")
 
         self.assertTrue(len(response.context['page_obj']) == 3)
 
-        
-        self.assertQuerysetEqual(
-            response.context['page_obj'],
-            [repr(x) for x in Expense.objects.filter(payment_time__month=month, payment_time__year=year)]
-        )
-    
-    def test_present_expense_with_multiple_expenses_more_than_page_objects(self):
+        self.assertQuerysetEqual(response.context['page_obj'], [
+            repr(x) for x in Expense.objects.filter(payment_time__month=month,
+                                                    payment_time__year=year)
+        ])
+
+    def test_present_expense_with_multiple_expenses_more_than_page_objects(
+            self):
         """
         expenses items should not exceed the paginated items allowed
         on the page
         """
         now = timezone.now()
         day, month, year = now.day, now.month, now.year
-        
+
         for _ in range(7):
             add_expense(amount=500)
 
-        response = self.client.get(reverse('expenses:index', args=(year, month)))
-        
+        response = self.client.get(
+            reverse('expenses:index', args=(year, month)))
+
         self.assertEqual(response.status_code, 200)
-        
+
         self.assertContains(response, "badge-expended")
         self.assertContains(response, "more than last month")
 
         self.assertTrue(len(response.context['page_obj']) == 6)
-    
-        self.assertQuerysetEqual(
-            response.context['page_obj'],
-            [repr(x) for x in Expense.objects.filter(payment_time__month=month, payment_time__year=year)[:6]]
-        )
+
+        self.assertQuerysetEqual(response.context['page_obj'], [
+            repr(x)
+            for x in Expense.objects.filter(payment_time__month=month,
+                                            payment_time__year=year)[:6]
+        ])
 
     def test_expense_intcomma(self):
         now = timezone.now()
@@ -278,17 +272,19 @@ class ExpenseIndexViewTests(TestCase):
 
         add_expense(amount=40500)
 
-        response = self.client.get(reverse('expenses:index', args=(year, month)))
+        response = self.client.get(
+            reverse('expenses:index', args=(year, month)))
         self.assertContains(response, '40,500.0', count=3)
-    
+
     def test_expense_intword_million(self):
         now = timezone.now()
         year, month = now.year, now.month
 
         add_expense(amount=1200000)
 
-        response = self.client.get(reverse('expenses:index', args=(year, month)))
-        
+        response = self.client.get(
+            reverse('expenses:index', args=(year, month)))
+
         # count=3, as no expenses in last month so (2 badges, 1 card header)
         self.assertContains(response, '1.2 million', count=3)
 
@@ -298,31 +294,77 @@ class ExpenseIndexViewTests(TestCase):
 
         add_expense(amount=1200000000)
 
-        response = self.client.get(reverse('expenses:index', args=(year, month)))
-        
+        response = self.client.get(
+            reverse('expenses:index', args=(year, month)))
+
         # count=3, as no expenses in last month so (2 badges, 1 card header)
         self.assertContains(response, '1.2 billion', count=3)
-    
+
+
 class ExpenseDetailViewTests(TestCase):
     def test_detail_no_expenses(self):
-        test_id = 1 # number doesn't matter as there is no item yet
-        response = self.client.get(reverse('expenses:detail', args=(1,)))
+        test_id = 1  # number doesn't matter as there is no item yet
+        response = self.client.get(reverse('expenses:detail', args=(1, )))
         self.assertEqual(response.status_code, 404)
-    
+
     def test_detail_expense(self):
         add_expense(amount=1534500, title='Custom Expense Test')
 
-        response = self.client.get(reverse('expenses:detail', args=(1,)))
+        response = self.client.get(reverse('expenses:detail', args=(1, )))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, '<h4 class="text-muted">Expense Title: Custom Expense Test</h4>')
+        self.assertContains(
+            response,
+            '<h4 class="text-muted">Expense Title: Custom Expense Test</h4>')
         self.assertContains(response, '1,534,500', count=1)
-    
 
 
+class ExpenseDeleteTests(TestCase):
+    def test_delete_present_expense(self):
+        now = timezone.now()
+        year, month = now.year, now.month
+        e = add_expense(amount=500)
+        add_expense(amount=500)
+        add_expense(amount=1000)
+        response = self.client.post(
+            reverse('expenses:delete_expense', args=(e.id, )))
+        self.assertEqual(response.status_code, 302)
+        index_response = self.client.get(
+            reverse('expenses:index', args=(year, month)))
+        self.assertContains(index_response,
+                            'Successfully <b>deleted</b> the requested item!')
+        self.assertContains(index_response, '1,500.0')
 
+
+class ExpenseUpdateTests(TestCase):
+    def test_update_no_expense(self):
+        test_id = 1
+        response = self.client.post(
+            reverse('expenses:update', args=(test_id, )))
+        self.assertEqual(response.status_code, 404)
+
+    def test_update_present_expense(self):
+        now = timezone.now()
+        year, month = now.year, now.month
+        e = add_expense(amount=500)
+        add_expense(amount=500)
+        add_expense(amount=1000)
+        path_to_post = reverse('expenses:update', args=(e.id, ))
+        response = self.client.post(path_to_post, data={
+            'price':1500,
+            'title':'updated test expense title',
+            'desc': 'updated test expense description'
+        })
+            
+        self.assertEqual(response.status_code, 302)
+
+        index_response = self.client.get(
+            reverse('expenses:index', args=(year, month)))
+        
+        self.assertContains(index_response,
+                            'Successfully <b>updated</b> the expense!')
+        self.assertContains(index_response, '3,000.0')
 
 # Remaining ------------------------
-# tests for --> update/edit (POST)
+# tests for --> update (POST)
 # tests for --> delete (POST)
-# tests for --> detail (GET)
 # tests for --> monthly chart (GET)
--- expenses/views.py
+++ expenses/views.py
@@ -11,58 +11,56 @@ from django.shortcuts import get_list_or_404, get_object_or_404, render
 from .models import Expense
 from django.utils import timezone
 
-
 # Create your views here.
+
 
 def home(request):
     now = timezone.now()
     year, month = now.year, now.month
 
-    return HttpResponseRedirect(reverse('expenses:index', args=(year,month,)))
+    return HttpResponseRedirect(reverse('expenses:index', args=(
+        year,
+        month,
+    )))
+
 
 def index(request, year_num, month_num):
     this_time = timezone.now()
     this_day, this_month, this_year = this_time.day, this_time.month, this_time.year
-    
-    requested_expenses = Expense.objects.filter(
-        payment_time__month=month_num,
-        payment_time__year=year_num
-        )
+
+    requested_expenses = Expense.objects.filter(payment_time__month=month_num,
+                                                payment_time__year=year_num)
 
     paginator = Paginator(requested_expenses, 6)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    requested_monthly_expense = sum([expense.amount for expense in requested_expenses])
+    requested_monthly_expense = sum(
+        [expense.amount for expense in requested_expenses])
 
     show_add_button = False
 
-    captured_date = datetime(
-        year=year_num,
-        month=month_num,
-        day=1,
-        tzinfo=timezone.get_current_timezone()
-        )
+    captured_date = datetime(year=year_num,
+                             month=month_num,
+                             day=1,
+                             tzinfo=timezone.get_current_timezone())
 
     if this_time - timedelta(days=this_day) <= captured_date <= this_time:
         show_add_button = True
-    
+
     prev_exp_month = month_num - 1
     prev_exp_year = year_num
-    
+
     if prev_exp_month == 0:
         prev_exp_month = 12
         prev_exp_year -= 1
-    
-    prev = {
-        'month':prev_exp_month,
-        'year':prev_exp_year
-    }
+
+    prev = {'month': prev_exp_month, 'year': prev_exp_year}
 
     last_monthly_expense = Expense.objects.filter(
         payment_time__month=prev_exp_month,
-        payment_time__year=prev_exp_year
-    ).aggregate(Sum('amount'))['amount__sum']
+        payment_time__year=prev_exp_year).aggregate(
+            Sum('amount'))['amount__sum']
 
     if not last_monthly_expense:
         last_monthly_expense = 0
@@ -81,13 +79,14 @@ def index(request, year_num, month_num):
         progress['color']['bg'] = 'same'
         progress['color']['text'] = 'light'
         progress['tail'] = 'Same as'
-    
+
     progress['tail'] += ' last month'
-    progress['difference'] = abs(requested_monthly_expense-last_monthly_expense)
+    progress['difference'] = abs(requested_monthly_expense -
+                                 last_monthly_expense)
 
     data = {
         # 'expenses': curr_expenses,
-        'prev':prev,
+        'prev': prev,
         'curr_monthly_expense': requested_monthly_expense,
         'last_monthly_expense': last_monthly_expense,
         'progress': progress,
@@ -100,9 +99,7 @@ def index(request, year_num, month_num):
 
 def detail(request, expense_id):
     expense_object = get_object_or_404(Expense, id=expense_id)
-    data = {
-        'expense': expense_object
-    }
+    data = {'expense': expense_object}
     return render(request, 'expenses/detail.html', context=data)
 
 
@@ -112,39 +109,48 @@ def add_expense(request):
         desc = request.POST.get('desc')
         price = float(request.POST.get('price'))
 
-        new_expense = Expense(title=title, description=desc, amount=price, payment_time=timezone.now())
-        
+        new_expense = Expense(title=title,
+                              description=desc,
+                              amount=price,
+                              payment_time=timezone.now())
+
         new_expense.save()
 
-        messages.add_message(request, level=SUCCESS, message='Successfully <b>added</b> the expense!', extra_tags='safe')
-    
+        messages.add_message(request,
+                             level=SUCCESS,
+                             message='Successfully <b>added</b> the expense!',
+                             extra_tags='safe')
+
         return HttpResponseRedirect(reverse('expenses:home'))
-    
+
     return render(request, 'expenses/add_expense.html')
 
 
 def delete_expense(request, expense_id):
-    if request.method=='POST':
+    if request.method == 'POST':
         expense = get_object_or_404(Expense, id=expense_id)
         expense_time = expense.payment_time
         month, year = expense_time.month, expense_time.year
 
         expense.delete()
 
-        messages.add_message(request, level=SUCCESS,
+        messages.add_message(
+            request,
+            level=SUCCESS,
             message='Successfully <b>deleted</b> the requested item!',
-            extra_tags='safe'
-        )
-        
-        return HttpResponseRedirect(reverse('expenses:index', args=(year,month)))
+            extra_tags='safe')
+
+        return HttpResponseRedirect(
+            reverse('expenses:index', args=(year, month)))
 
     else:
-        messages.add_message(request, level=messages.ERROR,
-            message='Invalid request to delete an item!',
-            extra_tags='safe'
-        )
+        messages.add_message(request,
+                             level=messages.ERROR,
+                             message='Invalid request to delete an item!',
+                             extra_tags='safe')
 
         return HttpResponseRedirect(reverse('expenses:home'))
+
 
 def update_expense(request, expense_id):
     if request.method == 'POST':
@@ -152,6 +158,9 @@ def update_expense(request, expense_id):
         title = request.POST.get('title')
         description = request.POST.get('desc')
         amount = request.POST.get('price')
+
+        expense_time = expense.payment_time
+        year, month = expense_time.year, expense_time.month
 
         if title:
             expense.title = title
@@ -162,13 +171,21 @@ def update_expense(request, expense_id):
 
         expense.save()
 
-        messages.add_message(request, level=SUCCESS, message='Successfully <b>updated</b> the expense!', extra_tags='safe')
+        messages.add_message(
+            request,
+            level=SUCCESS,
+            message='Successfully <b>updated</b> the expense!',
+            extra_tags='safe')
 
-        return HttpResponseRedirect(reverse('expenses:home'))
-    
+        return HttpResponseRedirect(
+            reverse('expenses:index', args=(
+                year,
+                month,
+            )))
+
     expense = get_object_or_404(Expense, id=expense_id)
 
-    return render (request, 'expenses/update_expense.html', {
+    return render(request, 'expenses/update_expense.html', {
         'expense': expense,
     })
 
@@ -184,38 +201,43 @@ def monthly_chart(request, year_num, month_num):
             payment_time__month=month_num,
             payment_time__year=year_num
         )
-    
+
     for expense in expenses_list:
         labels.append(expense.get('payment_time__date'))
         data.append(expense.get('total_expenses'))
-    
-    return render(request, 'expenses/month_chart.html', {
-        'labels': labels,
-        'data': data,
-        'req_date': datetime(year=year_num, month=month_num, day=1)
-    })
+
+    return render(
+        request, 'expenses/month_chart.html', {
+            'labels': labels,
+            'data': data,
+            'req_date': datetime(year=year_num, month=month_num, day=1)
+        })
 
 
 def delete_expenses_monthly(request, year_num, month_num):
-    if request.method=='POST':
-        expenses = get_list_or_404(
-            Expense, payment_time__month=month_num, payment_time__year=year_num
-        )
+    if request.method == 'POST':
+        expenses = get_list_or_404(Expense,
+                                   payment_time__month=month_num,
+                                   payment_time__year=year_num)
 
         for expense in expenses:
             expense.delete()
-        
-        messages.add_message(request, level=SUCCESS,
-            message='Successfully <b>deleted</b> all of the expense in the requested month!',
-            extra_tags='safe'
-        )
 
-        return HttpResponseRedirect(reverse('expenses:index', args=(year_num, month_num)))
+        messages.add_message(
+            request,
+            level=SUCCESS,
+            message=
+            'Successfully <b>deleted</b> all of the expense in the requested month!',
+            extra_tags='safe')
+
+        return HttpResponseRedirect(
+            reverse('expenses:index', args=(year_num, month_num)))
 
     else:
-        messages.add_message(request, level=messages.ERROR,
-            message='Invalid request to delete items!',
-            extra_tags='safe'
-        )
+        messages.add_message(request,
+                             level=messages.ERROR,
+                             message='Invalid request to delete items!',
+                             extra_tags='safe')
 
-        return HttpResponseRedirect(reverse('expenses:index', args=(year_num, month_num)))
+        return HttpResponseRedirect(
+            reverse('expenses:index', args=(year_num, month_num)))
